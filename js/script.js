4d4e6df98f79cc3eebadb7c6c8a146a0bab38e06
Andro D Nacu <andro.d.nacu@boeing.com>
2018-05-17 17:38:04 -0700

    Add new page and update CSS

--- js/script.js
+++ js/script.js
@@ -1,9 +1,23 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+      x.className += " responsive";
+  } else {
+      x.className = "topnav";
+  }
+}
+
+/* Contact Form Handler */
+
+function getEmail() {
+  event.preventDefault()
+  var name = document.contact.fullname.value
+  document.contact.fullname.value = ""
+  var email= document.contact.email.value
+  document.contact.email.value = ""
+  var area = document.contact.area.value
+  document.contact.area.value = ""
+  var feedback = document.contact.feedback.value
+  document.contact.feedback.value = ""
 }
